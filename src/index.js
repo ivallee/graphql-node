a4f17a416151a335a70355eae242114ba4e617c5
ivallee <isaacvallee@gmail.com>
2018-02-15 13:09:01 -0800

    create and implement subscriptions

--- src/index.js
+++ src/index.js
@@ -4,11 +4,13 @@ const { Prisma } = require('prisma-binding')
 // Resolvers
 const Query = require('./resolvers/Query');
 const Mutation = require('./resolvers/Mutation');
+const Subscription = require('./resolvers/Subscription');
 
 
 const resolvers = {
   Query,
-  Mutation
+  Mutation,
+  Subscription
 }
 
 const server = new GraphQLServer({
